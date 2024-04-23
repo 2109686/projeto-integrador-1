acab513296245e96d17c65065f61136921004b55
Manoel Lima <manupljr@gmail.com>
2024-04-22 23:52:32 -0300

    feat: add news controllers, models and migrations to project

---
+++ app/Http/Controllers/Admin/SupportController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Support;
+use Illuminate\Http\Request;
+
+class SupportController extends Controller
+{
+    public function index (Support $support)
+    {
+        /**
+         * Pode ser feito dessa forma, instaciando o objeto da Model
+         * $support = new Support();
+         * $supports = $support->all();
+         *
+         * Outra forma de executar, é chamando direto como abaixo.
+         * $supports = Support::all();
+         *
+         * Mas o ideal é utilizando a injeção de dependencia
+         */
+
+        $supports = $support->all();
+        dd($supports);
+
+        /**
+         * Pode enviar para a view passando o array ou utilizando o compact
+         *
+         * return view('admin.supports.index', [
+         *     'supports' => $supports
+         * ]);
+         *
+         */
+
+        return view('admin.supports.index', compact('supports'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\{SupportController};
+use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/contato', function () {
+//     return view('site.contact');
+// });
+
+Route::get('/contato', [SiteController::class, 'contact']);
+
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
