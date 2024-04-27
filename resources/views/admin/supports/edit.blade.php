09b61be54ea70a3d8cde58d81f8909b00777304d
Manoel Lima <manupljr@gmail.com>
2024-04-27 14:51:56 -0300

    feat: organize views using partials and components

--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,17 +1,12 @@
 <h1>Dúvida {{ $support->id }}</h1>
 
-@if ($errors->any)
-    @foreach ($errors->all() as $error)
-        {{ $error }}
-    @endforeach
-@endif
+<x-alert/>
 
 <form action="{{ route('supports.update', $support->id ) }}" method="POST">
     {{-- <input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}"> --}}
-    @csrf()
     @method('put')
     {{-- <input type="text" value="PUT" name="_method"> --}}
-    <input type="text" placeholder="Assunto" name="subject" value="{{ $support->subject }}">
-    <textarea name="body" id="body" cols="30" rows="5">{{ $support->body }}</textarea>
-    <button type="submit">Alterar</button>
+    @include('admin.supports.partials.form', [
+        'support' => $support
+    ])
 </form>
