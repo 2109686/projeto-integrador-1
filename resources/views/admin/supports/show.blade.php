66127d76787ca2208e440044414f49aa7f715573
Manoel Lima <manupljr@gmail.com>
2024-04-26 23:52:17 -0300

    feat: add delete support feature to project

--- resources/views/admin/supports/show.blade.php
+++ resources/views/admin/supports/show.blade.php
@@ -5,3 +5,11 @@
     <li>Status: {{ $support->status }}</li>
     <li>Descrição: {{ $support->body }}</li>
 </ul>
+
+<form action="{{ route('supports.destroy', $support->id ) }}" method="POST">
+    {{-- Muito importante em forms usar a diretiva @csrf porque senão vai dar erro 419 --}}
+    {{-- <input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}"> --}}
+    @csrf()
+    @method('delete')
+    <button type="submit">Deletar</button>
+</form>
--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,4 @@ Route::post('/supports', [SupportController::class, 'store'])->name('supports.st
 Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
 Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
 Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
+Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
