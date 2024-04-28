021c81c1261be3ac11a3772c20eac1d941aa65d6
Manoel Lima <manupljr@gmail.com>
2024-04-28 20:48:17 -0300

    feat: add enum to model and dto and fix pagination

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -42,7 +42,7 @@ class SupportController extends Controller
          //$supports = $this->service->getAll($request->filter);
          $supports = $this->service->paginate(
             page: $request->get('page', 1),
-            totalPerPage: $request->get('per_page', 1),
+            totalPerPage: $request->get('per_page', 15),
             filter: $request->filter,
          );
 
