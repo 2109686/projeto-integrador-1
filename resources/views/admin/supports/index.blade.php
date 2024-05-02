e1fe8409fa34fa737d6c77686b56592baf2f10bd
Manoel Lima <manupljr@gmail.com>
2024-05-02 20:03:36 -0300

    chore: refactor using template and create header

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,36 +1,45 @@
-<h1>Listagem dos Suportes</h1>
+@extends('admin.layouts.app')
 
-<a href="{{ route('supports.create') }}">Criar dúvida</a>
+@section('title', 'Fórum')
 
-<!--
-    Essa forma a blade dá um echo.
-    \{\!\! $supports \!\!\}
-    Usar com cuidado pois essa forma não previne ataques do tipo xss
+@section('header')
+    @include('admin.supports.partials.header', compact('supports'))
+@endsection
 
-    A forma mais segura é utilizando \{\{ $supports \}\}
--->
+@section('content')
 
-<table>
-    <thead>
-        <th>Assunto</th>
-        <th>Status</th>
-        <th>Descrição</th>
-        <th></th>
-    </thead>
-    <tbody>
-        @foreach ($supports->items() as $support)
-            <tr>
-                <td>{{ $support->subject }}</td>
-                <td>{{ getStatusSupport($support->status); }}</td>
-                <td>{{ $support->body }}</td>
-                <td>
-                    <a href="{{ route('supports.show', $support->id) }}">Ver</a>
-                    <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
-                </td>
-            </tr>
-        @endforeach
+    <a href="{{ route('supports.create') }}">Criar dúvida</a>
 
-    </tbody>
-</table>
+    <!--
+        Essa forma a blade dá um echo.
+        \{\!\! $supports \!\!\}
+        Usar com cuidado pois essa forma não previne ataques do tipo xss
 
-<x-pagination :paginator="$supports" :appends="$filters" />
+        A forma mais segura é utilizando \{\{ $supports \}\}
+    -->
+
+    <table>
+        <thead>
+            <th>Assunto</th>
+            <th>Status</th>
+            <th>Descrição</th>
+            <th></th>
+        </thead>
+        <tbody>
+            @foreach ($supports->items() as $support)
+                <tr>
+                    <td>{{ $support->subject }}</td>
+                    <td>{{ getStatusSupport($support->status); }}</td>
+                    <td>{{ $support->body }}</td>
+                    <td>
+                        <a href="{{ route('supports.show', $support->id) }}">Ver</a>
+                        <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
+                    </td>
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+
+    <x-pagination :paginator="$supports" :appends="$filters" />
+@endsection
